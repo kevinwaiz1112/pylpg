e6d7fbbe2e0ac335a44a7901ed6bfcbc56f973c8
kevin_waiz <kevin.waiz@dlr.de>
2024-03-22 15:50:40 +0100

    Code improvement

--- pylpg/simulation_setup.py
+++ pylpg/simulation_setup.py
@@ -26,13 +26,12 @@ def update_status_file(status_file, building_id):
     with open(status_file, 'a') as file:
         file.write(f"{building_id}\n")
 
-def simulate_building(building_id, households, startdate, enddate, output_folder, status_file, resolution):
+def simulate_building(building_id, households, startdate, enddate, output_folder, calc_folder, status_file, resolution):
     all_households = list(households.values())
 
     # Aktualisiere den Ausgabeordner für dieses Gebäude
     output_folder = os.path.join(output_folder, f"Results_{building_id}")
     # Directory in welchem die Berechnung durchgeführt wird; Wird nach Ende der Sim gelöscht
-    calc_folder = r"C:\03_Repos\sekquasens_interfaces\pylpg\pylpg"
 
     # Führen Sie lpg_execution.execute_lpg_with_many_householdata für das aktuelle Gebäude durch
     df = lpg_execution.execute_lpg_with_many_householdata(
@@ -51,63 +50,47 @@ def simulate_building(building_id, households, startdate, enddate, output_folder
 
     update_status_file(status_file, building_id)
 
-    # Löschen der nicht weiter benötigten Dateien (falls vorhanden)
-    file_to_delete = os.path.join(output_folder, "profilegenerator.copy.db3")
-    if os.path.exists(file_to_delete):
-        os.remove(file_to_delete)
+    # Liste der zu löschenden Dateien und Ordner
+    files_and_folders_to_delete = [
+        os.path.join(output_folder, "profilegenerator.copy.db3"),
+        os.path.join(output_folder, "finished.flag"),
+        os.path.join(output_folder, "Temporary Files"),
+        os.path.join(calc_folder, "C1_" + str(building_id)),
+        os.path.join(calc_folder, "LPG_win_" + str(building_id))
+    ]
 
-    file_to_delete = os.path.join(output_folder, "finished.flag")
-    if os.path.exists(file_to_delete):
-        os.remove(file_to_delete)
-
-    folder_to_delete = os.path.join(output_folder, "Temporary Files")
-    if os.path.exists(folder_to_delete):
-        shutil.rmtree(folder_to_delete)
-
-    folder_to_delete = os.path.join(calc_folder, "C" + "1_" + str(building_id))
-    if os.path.exists(folder_to_delete):
-        shutil.rmtree(folder_to_delete)
-
-    folder_to_delete = os.path.join(calc_folder, "LPG_win_"+ str(building_id))
-    if os.path.exists(folder_to_delete):
-        shutil.rmtree(folder_to_delete)
+    # Durchlaufen der Liste und Löschen der Dateien/Ordner, falls vorhanden
+    for path in files_and_folders_to_delete:
+        if os.path.exists(path):
+            if os.path.isfile(path):
+                os.remove(path)  # Löschen einer Datei
+            elif os.path.isdir(path):
+                shutil.rmtree(path)  # Löschen eines Ordners
 
 
-"""
-    from simulation_setup import *
+def save_annual_requirements_to_csv(building_id, output_folder, resolution):
+    seconds = resolution_to_seconds(resolution)
+    water_types = ['Cold Water', 'Warm Water', 'Hot Water']
+    results = {}
 
-    # Simulationsparameter
-    csv_filename_persons = r"C:\03_Repos\sekquasens_interfaces\pylpg\Data\persons_moabit_oneBuilding.csv"  # Ersetzen Sie dies durch den tatsächlichen Dateinamen
-    output_folder = r"C:\03_Repos\sekquasens_interfaces\pylpg\Data\Results"
-    startdate = "01.01.2024"  # Wichtig: MM.TT.JJJJ
-    enddate = "01.01.2024"
-    start_time = time.time()
-    resolution = "01:00:00"
-    num_processes = 1  # Anzahl der parallel auszuführenden Prozesse = Anzahl Kerne (RAM beachten !)
+    # Berechne den jährlichen Bedarf für Strom
+    electricity_file_path = os.path.join(output_folder, f"Results_{building_id}", f"Results",
+                                         f"SumProfiles_{seconds}s.House.Electricity.csv")
+    df_electricity = pd.read_csv(electricity_file_path, delimiter=';')
+    results['Electricity_kWh'] = calculate_annual_requirement(df_electricity.rename(columns={'Sum [kWh]': 'Sum'}))
 
-    # Erstellt die benötigten Haushalte pro Gebäude mithilfe der Templates und Klassen des LPG
-    household_data = LPG_sekquasens_coupling(csv_filename_persons)
-    # Erstelle eine Liste von Argumenten für die Gebäudesimulationen
-    status_file = os.path.join(output_folder, "simulation_status.txt")
-    force_resimulate_all = False  # Setzen Sie dies auf True, um alle Gebäude neu zu simulieren
-    completed_buildings = get_completed_buildings(status_file)
-    building_simulations_args = []
-    for building_id, households in household_data.items():
-        if force_resimulate_all or building_id not in completed_buildings:
-            building_simulations_args.append(
-                (building_id, households, startdate, enddate, output_folder, status_file, resolution, force_resimulate_all))
+    # Berechne den jährlichen Bedarf für Wasser (kalt, warm, heiß)
+    for water_type in water_types:
+        water_file_path = os.path.join(output_folder, f"Results_{building_id}", f"Results",
+                                       f"SumProfiles_{seconds}s.House.{water_type}.csv")
+        if os.path.exists(water_file_path):
+            df_water = pd.read_csv(water_file_path, delimiter=';')
+            results[f'{water_type.replace(" ", "_")}_L'] = calculate_annual_requirement(
+                df_water.rename(columns={'Sum [L]': 'Sum'}))
 
-    # Erstelle einen Pool von Prozessen, um die Simulationen parallel auszuführen
-    pool = multiprocessing.Pool(processes=num_processes)
-    # Führe die Simulationen parallel aus
-    pool.starmap(simulate_building, building_simulations_args)
-    # Beende den Pool und warte auf das Ende aller Prozesse
-    pool.close()
-    pool.join()
-
-    # Dauer der Simulation
-    end_time = time.time()
-    execution_time = end_time - start_time
-    print(f"Das Skript hat {execution_time} Sekunden gedauert.")
-
-"""
+    # Speichere die Ergebnisse in einer CSV-Datei
+    with open(os.path.join(output_folder, f"Results_{building_id}", "annual_requirements.csv"), "a") as file:
+        header = "Gebaeude-ID,Electricity_kWh,Cold_Water_L,Warm_Water_L,Hot_Water_L\n"
+        file.write(header)
+        data_line = f"{building_id},{results.get('Electricity_kWh', 0)},{results.get('Cold_Water_L', 0)},{results.get('Warm_Water_L', 0)},{results.get('Hot_Water_L', 0)}\n"
+        file.write(data_line)
